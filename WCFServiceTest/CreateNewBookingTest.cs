ac57e65638247bba75c009c08c20aad29a4110dc
DoomMachine <DoomMachine@DESKTOP-85M7CGS>
2020-04-22 21:15:19 +0200

    Test for Create Booking og NewRoom.

---
+++ WCFServiceTest/CreateNewBookingTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLayer;
+using ProductService;
+
+namespace WCFServiceTest
+{
+    [TestClass]
+    public class CreateNewBookingTest
+    {
+        [TestMethod]
+        public void CreateBooking ( ){
+
+            IBookingServices bs = new BookingServices();
+            ICustomerServices cs = new CustomerServices();
+            IEscapeRoom_Services es = new EscapeRoom_Services();
+            IEmplyeeServices ems = new EmplyeeServices();
+
+            ModelLayer.Customer cus = cs.Get("Anorak");
+            ModelLayer.Employee em = ems.Get(1);
+            ModelLayer.EscapeRoom er = es.GetForOwner(1);
+            ModelLayer.Customer customer = new Customer() {
+                customerNo = cus.customerNo,
+                firstName = cus.firstName,
+                lastName = cus.lastName,
+                mail = cus.mail,
+                password = cus.password,
+                phone = cus.phone,
+                username = cus.username
+            };
+            Employee employee = new Employee() {
+                address = em.address,
+                cityName = em.cityName,
+                employeeID = em.employeeID,
+                firstName = em.firstName,
+                lastName = em.lastName,
+                mail = em.mail,
+                phone = em.phone,
+                region = em.region,
+                zipcode = em.zipcode
+            };
+            EscapeRoom escapeRoom = new EscapeRoom() {
+                cleanTime = er.cleanTime,
+                description = er.description,
+                escapeRoomID = er.escapeRoomID,
+                maxClearTime = er.maxClearTime,
+                name = er.name,
+                price = er.price,
+                rating = er.rating
+            };
+            Booking hostBook;
+            Booking book = new Booking() {
+                amountOfPeople = 22,
+                bookingTime = DateTime.Now,
+                cus = customer,
+                date = DateTime.Now.AddDays(7.0).Date,
+                emp = employee,
+                er = escapeRoom
+            };
+
+
+            //Act
+            bs.Create(book.emp.employeeID, book.cus.username, book.er.escapeRoomID, book.bookingTime, book.amountOfPeople, book.date);
+            hostBook = bs.Get(book.er.escapeRoomID, book.cus.username, book.date);
+
+            //Assert
+            Assert.AreEqual(book.date, hostBook.date);
+            Assert.AreEqual(book.emp.employeeID, hostBook.emp.employeeID);
+            Assert.AreEqual(book.cus.username, hostBook.cus.username);
+
+            bs.Delete(book.emp.employeeID, book.cus.username, book.er.escapeRoomID, book.bookingTime, book.amountOfPeople, book.date);
+        }
+    }
+}
---
+++ WCFServiceTest/CreateNewRoomTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductService;
+
+namespace WCFServiceTest
+{
+    [TestClass]
+    public class CreateNewRoomTest
+    {
+        [TestMethod]
+        public void CreateRoom ( )
+        {
+
+            //Arrange
+            IEscapeRoom_Services ess = new EscapeRoom_Services();
+            IEmplyeeServices empSer = new EmplyeeServices();
+
+            ModelLayer.EscapeRoom Es1 = new ModelLayer.EscapeRoom();
+            ModelLayer.Employee Em1 = empSer.Get(1);
+            Es1.name = "The Dark Room";
+            Es1.description = "The Fors is Strong in the darkside";
+            Es1.maxClearTime = 180;
+            Es1.cleanTime = 90;
+            Es1.price = 1200;
+            Es1.rating = 0;
+            Es1.emp = Em1;
+
+            //Act
+            ess.CreateEscapeRoom(Es1.name, Es1.description, Es1.maxClearTime, Es1.cleanTime, Es1.price, Es1.rating, Es1.emp.employeeID);
+
+            //Assert
+            List<ModelLayer.EscapeRoom> escapeRooms = new List<ModelLayer.EscapeRoom>();
+            escapeRooms = (List<ModelLayer.EscapeRoom>)ess.GetAllForOwner();
+            bool found = false;
+            int i = 0;
+            ModelLayer.EscapeRoom EsR = new ModelLayer.EscapeRoom();
+
+            while (i < escapeRooms.Count && !found)
+            {
+                if (escapeRooms.ElementAt(i).name.Equals(Es1.name))
+                {
+                    EsR = escapeRooms.ElementAt(i);
+                    found = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            Assert.IsTrue(found);
+
+            if (EsR.escapeRoomID != 0)
+            {
+                ess.DeleteEscapeRoom(EsR.escapeRoomID);
+            }
+        }
+    }
+}
