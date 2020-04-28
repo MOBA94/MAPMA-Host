da63900b0cbcd49360f9ce174c9494c59f94c166
DoomMachine <DoomMachine@DESKTOP-85M7CGS>
2020-04-28 10:43:20 +0200

    Salt og hashing blev tilføjet og testet (Virker).

--- ProductService/DataAccessLayer/DBCustomer.cs
+++ ProductService/DataAccessLayer/DBCustomer.cs
@@ -5,9 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using ModelLayer;
 using System.Data.SqlClient;
+using System.Security.Cryptography;
 
 namespace ProductService.DataAccessLayer {
-    class DBCustomer : ICUSTOMER<Customer> {
+    public class DBCustomer : ICUSTOMER<Customer> {
 
         private string _connectionString;
 
@@ -44,12 +45,46 @@ namespace ProductService.DataAccessLayer {
             return cus;
         }
 
-        public IEnumerable<Customer> GetAll() {
+        public IEnumerable<Customer> GetAll() { 
             throw new NotImplementedException();
         }
 
         public void Update(Customer entity) {
             throw new NotImplementedException();
         }
+
+        public static string CreateSalt() {
+            var rngCsp = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[100];
+            rngCsp.GetBytes(salt);
+            return Convert.ToBase64String(salt);
+        }
+
+        public static string PasswordHashAndSalt(string password, string salt) {
+            
+            var password_byes = ASCIIEncoding.ASCII.GetBytes(password);
+
+            byte[] data_input = new byte[salt.Length + password_byes.Length];
+
+            SHA512 shaM = new SHA512Managed();
+            var hashed_byte_array = shaM.ComputeHash(data_input);
+
+            string hashed_result = Convert.ToBase64String(hashed_byte_array);
+
+            return hashed_result;
+        }
+
+        public static bool VerifyHashedPassword(string hashedPassword, string password, string salt) {
+
+            string newHashedPin = PasswordHashAndSalt(password, salt);
+            if (newHashedPin.Equals(hashedPassword)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+            }
+
+        }
     }
-}
+
---
+++ WCFServiceTest/SaltAndHashTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WCFServiceTest
+{
+    [TestClass]
+    public class SaltAndHashTest
+    {
+        [TestMethod]
+        public void TestMethod1 ( )
+        {
+            //Arrange
+            string password = "gruppe2ErSeje";
+            string salt;
+
+
+
+
+                //Act
+                salt = ProductService.DataAccessLayer.DBCustomer.CreateSalt();
+            string hashedPassword = ProductService.DataAccessLayer.DBCustomer.PasswordHashAndSalt(password, salt);
+            
+
+
+            //Assert
+            Assert.AreNotEqual(hashedPassword, password);
+            Assert.IsTrue(ProductService.DataAccessLayer.DBCustomer.VerifyHashedPassword(hashedPassword, password, salt));
+           
+        }
+    }
+}
